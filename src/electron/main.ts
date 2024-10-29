d345338522c7c006eca2ddad2b1139781961f716
Theo McMullien <theomcmullien.16@gmail.com>
2024-10-29 21:17:31 +1100

    read system resources

--- src/electron/main.ts
+++ src/electron/main.ts
@@ -1,12 +1,16 @@
 import { app, BrowserWindow } from 'electron';
 import path from 'path';
 import { isDev } from './util.js';
+import { pollResource } from './resourceManager.js';
 
 app.on('ready', () => {
     const mainWindow = new BrowserWindow({});
+
     if (isDev()) {
         mainWindow.loadURL('http://localhost:5123');
     } else {
         mainWindow.loadFile(path.join(app.getAppPath(), '/dist-react/index.html'));
     }
+
+    pollResource();
 });
